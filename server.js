36d1aed9640c2b16abe1f371a4e1015cd4af12b5
Michał Matysiak <michalmatysiak02@gmail.com>
2018-05-20 13:29:48 +0200

    Change file server.js

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var fs = require("fs");
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 var app = express();
 var stringifyFile;
 
 app.use(bodyParser.json());
 
-app.get("/getNote", function(req, res) {
-	fs.readFile("./test.json", "utf8", function(err, data) {
+app.get('/getNote', function(req, res) {
+	console.log('Otrzymanie żądania GET');
+	fs.readFile('./test.json', 'utf-8', function(err, data) {
 		if (err) throw err;
 		stringifyFile = data;
+		res.send(data);
+	});
+});
+
+app.post('/updateNote/:note', function(req, res) {
+	console.log('Otrzymanie żądania POST');
+	stringifyFile = req.params.note;
+	fs.writeFile('./test.json', stringifyFile, function(err) {
+		if (err) throw err;
+		console.log('file updated');
 		res.send(stringifyFile);
 	});
 });
 
-app.post("/updateNote/:note", function(req, res) {
-	fs.readFile(".test.json", "utf8", function(err, data) {
-		stringifyFile = data + req.params.note;
-		res.send(data);
-		fs.writeFile("./test.json", stringifyFile, function(err) {
-			if (err) throw err;
-			console.log("file updated");
-			res.send(stringifyFile);
-		});
-	});
-});
-
 var server = app.listen(3000, function() {
-	console.log("Przykładowa aplikacja nasłuchuje na http://localhost:3000");
-});
+	console.log('Aplikacja nasłuchuje http://localhost:3000');
+})
+
+
+
