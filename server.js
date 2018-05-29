19d126d6947361864533e88ac8b3726ff9223d40
Michał Matysiak <michalmatysiak02@gmail.com>
2018-05-29 16:36:10 +0200

    change test.json and server.js

--- server.js
+++ server.js
@@ -16,13 +16,23 @@ app.get('/getNote', function(req, res) {
 });
 
 app.post('/updateNote/:note', function(req, res) {
-	console.log('Otrzymanie żądania POST');
-	stringifyFile = req.params.note;
-	fs.writeFile('./test.json', stringifyFile, function(err) {
+	fs.readFile('./test.json', 'utf-8', function(err, data) {
 		if (err) throw err;
-		console.log('file updated');
-		res.send(stringifyFile);
+
+		var jsonObject = JSON.parse(data);
+		jsonObject.notes.push(req.params.note);
+		var json = JSON.stringify(jsonObject);
+		fs.writeFile('./test.json', json, function(err) {
+			res.send(jsonObject);
+		})
+		
 	});
+	// console.log('Otrzymanie żądania POST');
+	// stringifyFile = req.params.note;
+	// fs.writeFile('./test.json', stringifyFile, function(err) {
+	// 	console.log('file updated');
+	// 	res.send(stringifyFile);
+	// });
 });
 
 var server = app.listen(3000, function() {
